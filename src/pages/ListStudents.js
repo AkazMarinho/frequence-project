ae645442e37189c0c24cf1da8641eb929313baad
Vicente-Ferrer <Vicenteferreira473@gmail.com>
2023-12-05 10:34:20 -0300

    feat: tela de listagem de estudantes

--- src/pages/ListStudents.js
+++ src/pages/ListStudents.js
@@ -1,38 +1,24 @@
-import { useState, useEffect } from "react"
-import axios from 'axios'
+import { useState, useEffect, useContext } from "react";
+import FlatList from "flatlist-react/lib";
+import { AuthContext } from "../context/userContext";
 
-export function ListStudents(){
-
-    const [listStudent, setListStudent] = useState([])
-
-
-
-    useEffect(() => {
-        axios.get("https://sfsystem.onrender.com/api/v1/admin/333530590131", {
-            params : {
-                'tenant' : '987653'
-            }, 
-            headers :{
-                'Content-Type' : 'application/json'
-            }
-        }
-        )
-        .then((data) => {
-            console.log(data.data)
-            setListStudent(data.data)
-        })
-        .catch((err) =>console.log(err))
-    }, [])
-
-
-    return(<div>
-        <div>
-        </div>
-        <p>
-            {listStudent.firstName}
-           
-        </p>
-    </div>
-    
-    )
+export function ListStudents() {
+  const [listStudent, setListStudent] = useState([]);
+  const { getStudentList, students } = useContext(AuthContext);
+  renderStudentList = (students) => {
+    return (
+      <li key={students.id}>
+        <b>{students.firstName} </b>
+      </li>
+    );
+  };
+  return (
+    <ul>
+      <FlatList
+        list={this.props.students}
+        renderItem={this.renderStudentList}
+        renderWhenEmpty={() => <div>Nenhuma aluno cadastrado!</div>}
+      />
+    </ul>
+  );
 }
