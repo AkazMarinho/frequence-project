a3c59b0edb0fd7b6c3df87f82b67308502454bce
AkazMarinho <akazmarinho@gmail.com>
2024-01-12 17:04:46 -0300

    parte que ta bugada

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import {Link } from "react-router-dom";
-import { useState } from 'react';
-import style from './App.module.css'
-import { ListStudents } from './pages/ListStudents';
 import { RoutesApp } from "./routes/Routes";
+import { NavBar } from "./pages/navbar/NavBar";
+import { Login } from "./pages/login/Login.js";
 
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "./context/AuthProvider.js";
+import Loader from "./Layout/Loader.js";
 
 function App() {
+  const {loader} =useContext(AuthContext)
+  const KeyAuth = localStorage.getItem("log");
 
-  // const [validation, setValidation] = useState()
+  const [validation, setValidation] = useState(KeyAuth)
+  const [jsx, setJsx] = useState('')
+
+  useEffect(() => {
+    setValidation(KeyAuth)
+    console.log("validation: " + KeyAuth);
+  },[KeyAuth])
+
+
 
   return (
-    
-      <div className='w-full h-[100vh]'>
-          {/* <Link to='/'>
-            Home
-          </Link>
 
-          <Link to='/login'>
-            login
-          </Link> */}
+      <div className='bg-[#f1f1f1] flex w-full h-full'>
+        
+        {loader && <Loader/>}
 
-        <RoutesApp/>
+        { validation == true ?
+          (<>
+            <NavBar/> 
+            <RoutesApp/> 
+            </>)
+        :
+            <Login/>
+        }
+        
       </div>
-    
   );
 }
 
